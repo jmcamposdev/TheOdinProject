75b1fd03dcdd0028ddce6739233e2d9885631ec0
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-05-29 12:09:53 +0200

    Test inputs event when keyup in RegexForm

--- Foundations/Exercises/3-RegexFormJS/validation.js
+++ Foundations/Exercises/3-RegexFormJS/validation.js
@@ -1,1 +1,10 @@
-console.log("is working");
+const inputs = document.querySelectorAll('input'); // Select all inputs 
+const pattern = {
+    username: /^[a-z\d]{5-12}$/
+}
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup', (e) => {
+        console.log("key pressed");
+    })
+})
