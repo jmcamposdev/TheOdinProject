29cefd17eebfaacb827fea1ccfa71407f95848c0
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-05-29 12:20:17 +0200

    Adding password pattern to RegexForm

--- Foundations/Exercises/3-RegexFormJS/validation.js
+++ Foundations/Exercises/3-RegexFormJS/validation.js
@@ -2,7 +2,7 @@ const inputs = document.querySelectorAll('input'); // Select all inputs
 const pattern = {
     username: /^[a-z\d]{5-12}$/,
     email: /^([a-z\d\.-]+)@([a-z\d\.-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
-    
+    password: /^[\w@-]{8,20}$/,
 }
 
 inputs.forEach((input) => {
