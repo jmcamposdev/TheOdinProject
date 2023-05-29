894b8e06b2895f2d6ef4e9333e7a5790914b5c97
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-05-29 12:45:30 +0200

    Implemented remove the classes if the input is Empty on RegexForm

--- Foundations/Exercises/3-RegexFormJS/validation.js
+++ Foundations/Exercises/3-RegexFormJS/validation.js
@@ -1,4 +1,5 @@
 const inputs = document.querySelectorAll('input'); // Select all inputs 
+// JSON object of regex patterns
 const pattern = {
     username: /^[a-z\d]{5-12}$/,
     email: /^([a-z\d\.-]+)@([a-z\d\.-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
@@ -7,17 +8,21 @@ const pattern = {
     slug: /^[a-z\d-]{8-20}$/,
 }
 
-function validation (field, pattern) {
-    if (pattern.test(field.value)) { 
-        field.className = "valid";
-
-    } else {
-        field.className = "invalid";
+// Validation function
+function validation (field, pattern) { // Pass in field and pattern
+    if (pattern.test(field.value)) {  // If pattern matches field value
+        field.className = "valid"; // Add valid class
+    } else { // If pattern doesn't match field value
+        field.className = "invalid"; // Add invalid class
     }
 }
 
 inputs.forEach((input) => {
     input.addEventListener('keyup', (e) => {
-        validation(input, pattern[input.attributes.name.value]);
+        if (!input.value) { // If input is empty
+            input.className = ""; // Remove class
+        } else { // If input is not empty
+            validation(input, pattern[input.attributes.name.value]); // Call validation function
+        }
     })
 })
