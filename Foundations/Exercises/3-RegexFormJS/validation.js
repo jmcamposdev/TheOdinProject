78b41dc57995e9fd06c521eacf7962a908edd545
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-05-29 12:48:12 +0200

    Fixed pattern range on RegexForm

--- Foundations/Exercises/3-RegexFormJS/validation.js
+++ Foundations/Exercises/3-RegexFormJS/validation.js
@@ -1,11 +1,11 @@
 const inputs = document.querySelectorAll('input'); // Select all inputs 
 // JSON object of regex patterns
 const pattern = {
-    username: /^[a-z\d]{5-12}$/,
+    username: /^[a-z\d]{5,12}$/,
     email: /^([a-z\d\.-]+)@([a-z\d\.-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
     password: /^[\w@-]{8,20}$/,
     telephone: /^\d{11}$/,
-    slug: /^[a-z\d-]{8-20}$/,
+    slug: /^[a-z\d-]{8,20}$/,
 }
 
 // Validation function
