530210d270a185db7be7eeebeb885bfa3c7962d7
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-05-29 12:21:07 +0200

    Added slug pattern to RegexForm

--- Foundations/Exercises/3-RegexFormJS/validation.js
+++ Foundations/Exercises/3-RegexFormJS/validation.js
@@ -4,6 +4,7 @@ const pattern = {
     email: /^([a-z\d\.-]+)@([a-z\d\.-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
     password: /^[\w@-]{8,20}$/,
     telephone: /^\d{11}$/,
+    slug: /^[a-z\d-]{8-20}$/,
 }
 
 inputs.forEach((input) => {
