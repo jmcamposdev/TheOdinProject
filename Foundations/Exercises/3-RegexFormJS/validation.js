2384348ec5c2f1494adddd54619caddac1e2eb23
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-05-29 12:20:48 +0200

    Added telephone pattern on RegexForm

--- Foundations/Exercises/3-RegexFormJS/validation.js
+++ Foundations/Exercises/3-RegexFormJS/validation.js
@@ -3,6 +3,7 @@ const pattern = {
     username: /^[a-z\d]{5-12}$/,
     email: /^([a-z\d\.-]+)@([a-z\d\.-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
     password: /^[\w@-]{8,20}$/,
+    telephone: /^\d{11}$/,
 }
 
 inputs.forEach((input) => {
