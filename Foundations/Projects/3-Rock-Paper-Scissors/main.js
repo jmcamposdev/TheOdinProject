bbef2a78f57f397b6006db8661472856af194258
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-05-29 20:11:19 +0200

    Implemented 5 round and ask the player to insert the data

--- Foundations/Projects/3-Rock-Paper-Scissors/main.js
+++ Foundations/Projects/3-Rock-Paper-Scissors/main.js
@@ -4,10 +4,36 @@ const TYPES = {
     2: "Scissors",
 };
 
-const playerSelection = 0;
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+game()
 
+
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let round = 1;
+    while (round <= 5) {
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+        const result = playRound(playerSelection, computerSelection);
+        if (result) {
+            playerScore++;
+            console.log(`You win! ${TYPES[playerSelection]} beats ${TYPES[computerSelection]}`);
+        } else {
+            computerScore++;
+            console.log(`You lose! ${TYPES[computerSelection]} beats ${TYPES[playerSelection]}`);
+        }
+        round++;
+    }
+
+    if (playerScore > computerScore) {
+        console.log("You win!");
+    } else if (playerScore < computerScore) {
+        console.log("You lose!");
+    } else {
+        console.log("It's a tie!");
+    }
+}
 
 function playRound(playerSelection, computerSelection) {
     let hasUserWin;
@@ -21,11 +47,12 @@ function playRound(playerSelection, computerSelection) {
         hasUserWin = computerSelection === 1; // If computer chose paper, user wins
     }
 
-    if (hasUserWin) {
-        return `You win! ${TYPES[playerSelection]} beats ${TYPES[computerSelection]}`;
-    } else {
-        return `You lose! ${TYPES[computerSelection]} beats ${TYPES[playerSelection]}`;
-    }
+    return hasUserWin;
+    // if (hasUserWin) {
+    //     return `You win! ${TYPES[playerSelection]} beats ${TYPES[computerSelection]}`;
+    // } else {
+    //     return `You lose! ${TYPES[computerSelection]} beats ${TYPES[playerSelection]}`;
+    // }
 }
 
 /**
@@ -34,4 +61,18 @@ function playRound(playerSelection, computerSelection) {
  */
 function getComputerChoice() {
     return Math.floor(Math.random() * 3);
+}
+
+function getPlayerChoice() {
+    let playerChoice = prompt("Rock, paper or scissors?").toLowerCase();
+    if (playerChoice === "rock") {
+        return 0;
+    } else if (playerChoice === "paper") {
+        return 1;
+    } else if (playerChoice === "scissors") {
+        return 2;
+    } else {
+        alert("Invalid choice!");
+        return getPlayerChoice();
+    }
 }
