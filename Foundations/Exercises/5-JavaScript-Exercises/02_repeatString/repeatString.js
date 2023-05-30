f7a9cedf5b7b2576d817cadc6b77987f1267a8a2
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-05-30 09:01:31 +0200

    Resolve the repeatString Exercises

--- Foundations/Exercises/5-JavaScript-Exercises/02_repeatString/repeatString.js
+++ Foundations/Exercises/5-JavaScript-Exercises/02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) { // negative numbers are not valid
+        return "ERROR";
+    }
 
+    // Generate a string with the given string repeated num times
+    let repeatedString = "";
+    for (let i = 0; i < num; i++) {
+        repeatedString += string;
+    }
+
+    return repeatedString;
 };
 
 // Do not edit below this line
