6351bdc61b04eae3148cf9ccafb970552d2d432a
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-05-30 09:05:27 +0200

    Resolve the reverseString Exercises

--- Foundations/Exercises/5-JavaScript-Exercises/03_reverseString/reverseString.js
+++ Foundations/Exercises/5-JavaScript-Exercises/03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // Convert string to array, reverse array, join all characters by empty string
+    return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
