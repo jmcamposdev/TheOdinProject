2cb7d8f4db804fc919912a8ed94442f746ad30e3
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-05-30 07:43:34 +0200

    Added the activity statement of Exercise3 - Loops

--- Foundations/Exercises/4-Loops/Exercise3/main.js
+++ Foundations/Exercises/4-Loops/Exercise3/main.js
@@ -1,3 +1,8 @@
+/*
+In our first looping task we want you to start by creating a simple loop
+that goes through all the items in the provided myArray and prints them out on the screen
+inside list items (i.e., <li> elements), which are appended to the provided list.
+ */
 const myArray = ['tomatoes', 'chick peas', 'onions', 'rice', 'black beans'];
 const list = document.createElement('ul');
 
