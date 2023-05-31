0f0cf8d807baf7a164487b34dcd7dde15faf52fb
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-05-31 22:35:33 +0200

    Implemented the grid, clear Button and color picker

--- Foundations/Projects/4-Etch-A-Sketch/main.js
+++ Foundations/Projects/4-Etch-A-Sketch/main.js
@@ -1,15 +1,42 @@
-const rows = 16;
-const columns = 16;
+let rows = 16;
+let columns = 16;
 const divGrid = document.querySelector('#grid');
-createGrid(rows, columns);
+const sliderGrid = document.querySelector('.slider');
+const sliderValue = document.querySelector('#grid-size');
+const pickColor = document.querySelector('#pick-color');
+console.log(sliderValue)
+
+createGrid();
 
 
-function createGrid(rows, columns) {
+function createGrid() {
+    divGrid.textContent = '';
     divGrid.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
     divGrid.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
     for (let i = 0; i < (rows * columns); i++) {
         let cell = document.createElement('div');
-        //cell.addEventListener('mouseover', changeColor);
+        cell.addEventListener('mouseover', changeColor);
         divGrid.appendChild(cell).className = 'grid-item';
     }
+}
+
+function changeColor(e) {
+    if (e.buttons === 1) {
+        let selectedColor = pickColor.value;
+        e.target.style.backgroundColor = selectedColor;
+    }
+}
+
+sliderGrid.addEventListener('change', function () {
+    rows = columns = sliderGrid.value;
+    createGrid()
+});
+
+sliderGrid.oninput = function () {
+    sliderValue.textContent = sliderGrid.value;
+}
+
+function clearGrid() {
+    const cells = document.querySelectorAll('.grid-item');
+    cells.forEach(cell => cell.style.backgroundColor = 'white');
 }
