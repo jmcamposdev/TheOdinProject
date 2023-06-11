d1e6a6ac3e78fb244b68a5e0bb09030e2a67a9a2
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-06-11 10:21:17 +0200

    Added operate functions

--- Foundations/Projects/5-Calculator/main.js
+++ Foundations/Projects/5-Calculator/main.js
@@ -11,3 +11,24 @@ function multiply (num1, num2) {
 function divide (num1, num2) {
     return num1 / num2;
 }
+
+function operate (num1, num2, operator) {
+    let result;
+    switch (operator) {
+        case "+":
+            result = add(num1, num2);
+             break;
+        case "-":
+            result = subtract(num1, num2);
+            break;
+        case "*":
+            result = multiply(num1, num2);
+            break;
+        case "/":
+            result = divide(num1, num2);
+            break
+        default:
+            break;
+    }
+    return result;
+}
