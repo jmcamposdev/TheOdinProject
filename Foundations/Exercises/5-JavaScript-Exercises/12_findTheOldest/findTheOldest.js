64473c3796871b6c277f621787ce7cbcc44362fa
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-06-11 09:57:27 +0200

    Finish findTheOldest Function, passed all Test

--- Foundations/Exercises/5-JavaScript-Exercises/12_findTheOldest/findTheOldest.js
+++ Foundations/Exercises/5-JavaScript-Exercises/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(listOfPeople) {
+    return listOfPeople.reduce((oldest, currentPerson) => {
+       const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+       const currentPersonAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+       return oldestAge < currentPersonAge ? currentPerson : oldest;
+    })
+};
 
+const getAge = function (birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
 };
 
 // Do not edit below this line
