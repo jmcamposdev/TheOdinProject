34b93452c98a1f45d44ed4cb2a5fdfb29505b59d
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-06-11 17:54:17 +0200

    Implemented print numbers and clear the main display

--- Foundations/Projects/5-Calculator/main.js
+++ Foundations/Projects/5-Calculator/main.js
@@ -1,3 +1,32 @@
+/* Get all the buttons */
+const operators = document.querySelectorAll(".operator");
+const numbers = document.querySelectorAll(".number");
+const clear = document.querySelector("#clear");
+const equals = document.querySelector("#equals");
+const decimal = document.querySelector("#decimal");
+const display = document.querySelector("#display");
+const displayHistory = document.querySelector("#display-history");
+clearDisplay();
+
+/* Add event listeners to all the buttons */
+numbers.forEach(number => {
+    number.addEventListener("click", () => {
+        displayNumber(number.textContent);
+    });
+});
+clear.addEventListener("click", clearDisplay);
+
+
+// Display Functions
+function displayNumber (number) {
+    if (display.textContent.length < 15) {
+        display.textContent += number;
+    }
+}
+function clearDisplay () {
+    display.textContent = "";
+}
+
 // Operation Functions
 function add (num1, num2) {
     return num1 + num2;
