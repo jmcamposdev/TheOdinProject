0a3e8651ff42accb5c895099a83a065e759d63f0
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-06-11 10:15:58 +0200

    Added main operations functions

--- Foundations/Projects/5-Calculator/main.js
+++ Foundations/Projects/5-Calculator/main.js
@@ -1,1 +1,13 @@
-console.log("Is working")
+// Operation Functions
+function add (num1, num2) {
+    return num1 + num2;
+}
+function subtract (num1, num2) {
+    return num1 - num2;
+}
+function multiply (num1, num2) {
+    return num1 * num2;
+}
+function divide (num1, num2) {
+    return num1 / num2;
+}
