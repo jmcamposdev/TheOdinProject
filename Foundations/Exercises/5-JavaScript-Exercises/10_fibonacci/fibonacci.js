96151a5458f051d09ac6504068222d18e51f0b6f
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-06-11 09:41:08 +0200

    Finish Fibonacci Function, passed all Test

--- Foundations/Exercises/5-JavaScript-Exercises/10_fibonacci/fibonacci.js
+++ Foundations/Exercises/5-JavaScript-Exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    number = +number; // Convert to number
+    if (number <= 0) {
+        return "OOPS";
+    }
+    let x1 = 1;
+    let x2 = 1;
+    let aux;
+    for (let i = 2; i < number; i++) {
+        aux = x1;
+        x1 += x2;
+        x2 = aux;
+    }
+    return x1;
 };
 
 // Do not edit below this line
