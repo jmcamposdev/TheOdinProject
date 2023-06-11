3d897c59ec95a0b380427fddbf85264825842e44
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-06-11 08:17:55 +0200

    Finish Palindromes Function, passed all Test

--- Foundations/Exercises/5-JavaScript-Exercises/09_palindromes/palindromes.js
+++ Foundations/Exercises/5-JavaScript-Exercises/09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (letter) {
+    let filterLetter = letter.replace(/[^a-zA-Z]/gm,"").toLowerCase();
+    let reverseLetter = reverseString(filterLetter);
+    return reverseLetter === filterLetter;
 };
+
+function reverseString (string) {
+    let reverseString = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+        reverseString += string[i];
+    }
+    return reverseString
+}
 
 // Do not edit below this line
 module.exports = palindromes;
