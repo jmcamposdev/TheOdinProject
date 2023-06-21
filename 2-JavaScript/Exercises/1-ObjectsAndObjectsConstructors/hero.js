d82905e80e69a92fba964152691ecb96eebf63b3
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-06-21 17:09:15 +0200

    Created the Constructor of Hero

---
+++ 2-JavaScript/Exercises/1-ObjectsAndObjectsConstructors/hero.js
@@ -0,0 +1,7 @@
+// This is an example of Prototype of an Object
+// First of all We need to create a constructor
+function Hero (name, level) {
+    // We use The this keyword will refer to the new instance that is created
+    this.name = name;
+    this.level = level;
+}
