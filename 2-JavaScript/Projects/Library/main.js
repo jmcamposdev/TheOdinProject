807932d817c5e2e1b369099cef62b78172a68511
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-06-22 17:25:57 +0200

    Added Header and Footer Style and Book class

--- 2-JavaScript/Projects/Library/main.js
+++ 2-JavaScript/Projects/Library/main.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+let myLibrary = [];
+class Book {
+    constructor(title, author, pageNumber, isRead) {
+        this.title = title;
+        this.author = author;
+        this.pageNumber = pageNumber;
+        this.isRead = isRead;
+    }
+    info() {
+        return `${this.title} by ${this.author}, ${this.pageNumber} pages, ${this.isRead ? 'read' : 'not read yet'}`;
+    }
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
