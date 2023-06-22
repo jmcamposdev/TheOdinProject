357a496197d4fd73daa5de40b9ea8b043debe103
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-06-22 16:27:18 +0200

    Added Second Example of Hero and Inheritance

--- 2-JavaScript/Exercises/1-ObjectsAndObjectsConstructors/hero.js
+++ 2-JavaScript/Exercises/1-ObjectsAndObjectsConstructors/hero.js
@@ -5,10 +5,36 @@ function Hero (name, level) {
     this.name = name;
     this.level = level;
 }
+// Warrior Constructor
+function Warrior (name, level, weapon) {
+    Hero.call(this, name, level);  // Call the parent constructor
 
-Hero.prototype.greet = () => {
-    return `${this.name} says hello.`;
+    this.weapon = weapon; // Add a new property
+}
+// Healer Constructor
+function Healer (name, level, spell) {
+    Hero.call(this, name, level);  // Call the parent constructor
+    this.spell = spell; // Add a new property
 }
 
-const hero1 = new Hero("Jose", 1);
+// Set the prototype of the child constructor to an instance of the parent constructor
+// Object.setPrototypeOf(Warrior.prototype, Hero.prototype);
+// Object.setPrototypeOf(Healer.prototype, Hero.prototype);
+
+// Add greet method to the Hero prototype
+Hero.prototype.greet = function() {
+    return `${this.name} says hello.`;
+}
+// Add attack method to the Warrior prototype
+Warrior.prototype.attack = function () {
+    return `${this.name} attacks with the ${this.weapon}.`;
+}
+// Add heal method to the Healer prototype
+Healer.prototype.heal = function () {
+    return `${this.name} casts ${this.spell}.`;
+}
+
+const hero1 = new Warrior("Bjorn", 1, "axe");
+const hero2 = new Healer("Maria", 1, "Healing Light");
+
 console.log(hero1.greet());
