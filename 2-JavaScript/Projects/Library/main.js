eac506452a76b1569cc563acb286ed155dae535d
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-06-22 20:03:28 +0200

    Implemented Add Book Button and Popup Script

--- 2-JavaScript/Projects/Library/main.js
+++ 2-JavaScript/Projects/Library/main.js
@@ -1,4 +1,3 @@
-let myLibrary = [];
 class Book {
     constructor(title, author, pageNumber, isRead) {
         this.title = title;
@@ -11,7 +10,33 @@ class Book {
     }
 }
 
+let myLibrary = [];
+myLibrary.push(new Book('The Hobbit', 'J.R.R. Tolkien', 295, true));
+myLibrary.push(new Book('The Fellowship of the Ring', 'J.R.R. Tolkien', 423, true));
+myLibrary.push(new Book('The Two Towers', 'J.R.R. Tolkien', 352, true));
+myLibrary.push(new Book('The Return of the King', 'J.R.R. Tolkien', 416, true));
+
+
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
+
+
+
+// Popup Script
+const popup = document.querySelector('.popup');
+const createBookBtn = document.querySelector('.create-book-btn');
+
+// Button Script
+createBookBtn.addEventListener('click', () => {
+    popup.classList.toggle('active');
+}
+);
+// Popup Script
+popup.addEventListener('click', (e) => {
+    if (e.target.classList.contains('popup')) {
+        popup.classList.toggle('active');
+    }
+}
+);
