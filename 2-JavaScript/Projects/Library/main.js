98ea0e5d8b08badc9c8843480d26356ccb8631dd
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-06-23 16:49:51 +0200

    Created displayBooks Functions

--- 2-JavaScript/Projects/Library/main.js
+++ 2-JavaScript/Projects/Library/main.js
@@ -11,16 +11,36 @@ class Book {
 }
 
 let myLibrary = [];
-myLibrary.push(new Book('The Hobbit', 'J.R.R. Tolkien', 295, true));
+myLibrary.push(new Book('The Hobbit', 'J.R.R. Tolkien', 295, false));
 myLibrary.push(new Book('The Fellowship of the Ring', 'J.R.R. Tolkien', 423, true));
-myLibrary.push(new Book('The Two Towers', 'J.R.R. Tolkien', 352, true));
+myLibrary.push(new Book('The Two Towers', 'J.R.R. Tolkien', 352, false));
 myLibrary.push(new Book('The Return of the King', 'J.R.R. Tolkien', 416, true));
-
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
+function displayBooks() {
+    const bookContainer = document.querySelector('.books-container');
+    bookContainer.innerHTML = '';
+    myLibrary.forEach((book, index) => {
+        const bookDiv = document.createElement('div');
+        bookDiv.classList.add('book');
+        book.isRead ? bookDiv.classList.add('read') : bookDiv.classList.add('not-read');
+        bookDiv.setAttribute('data-index', index);
+        bookDiv.innerHTML = `
+        <div class="content">
+            <div class="book-title">${book.title}</div>
+            <div class="book-author">${book.author}</div>
+            <div class="book-pages">${book.pageNumber} pages</div>
+            <button class="read-button">Not Read</button>
+        </div>
+        `;
+        bookContainer.appendChild(bookDiv);
+    });
+}
+
+displayBooks();
 
 
 
