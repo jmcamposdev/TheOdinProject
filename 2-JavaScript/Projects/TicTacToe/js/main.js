a86ed2699ba2fadc6c5a6a675aec638b63bde0dd
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-06-26 18:48:24 +0200

    Implemented Winner and Reset Functions and Functionality

--- 2-JavaScript/Projects/TicTacToe/js/main.js
+++ 2-JavaScript/Projects/TicTacToe/js/main.js
@@ -2,13 +2,18 @@ import Player  from "./player.js";
 import ticTacToe from "./tictactoe.js";
 
 // Elements
+const restartBtn = document.querySelector("#restart");
 const roundCounter = document.querySelector("#round-counter");
 const modalText = document.querySelector(".modal-text");
+const player1Points = document.querySelector("#player1-points");
+const player2Points = document.querySelector("#player2-points");
 
 const player1 = new Player("Player 1", "X");
 const player2 = new Player("Player 2", "O");
 let currentPlayer = player1;
 
+// Event listeners
+restartBtn.addEventListener("click", restartGame);
 const tabs = document.querySelectorAll(".tab");
 tabs.forEach(tab => {
     tab.addEventListener("click", (e) => {
@@ -20,6 +25,14 @@ tabs.forEach(tab => {
 
         ticTacToe.placeCard(card, currentPlayer.symbol, index);
         
+        if (ticTacToe.isWinner(currentPlayer.symbol)) {
+            setModalText(`${currentPlayer.name} wins!`);
+            ticTacToe.cleanBoard();
+            addRound();
+            addPlayerPoints(currentPlayer);
+            return;
+        }
+
         if (ticTacToe.isDraw()) {
             setModalText("Draw!");
             ticTacToe.cleanBoard();
@@ -33,10 +46,31 @@ tabs.forEach(tab => {
 
 ticTacToe.buildBoard();
 
+function restartGame() {
+    ticTacToe.cleanBoard();
+    player1.points = 0;
+    player2.points = 0;
+    updatePlayerPoints();
+    setRound(0);
+    setModalText("");
+}
+
+function setRound(round) {
+    roundCounter.textContent = round;
+}
 function addRound() {
     roundCounter.textContent = Number(roundCounter.textContent) + 1;
 };
 
+function addPlayerPoints(player) {
+    player.points += 1;
+    updatePlayerPoints();
+}
+
+function updatePlayerPoints() {
+    player1Points.textContent = player1.points;
+    player2Points.textContent = player2.points;
+}
 function setModalText(text) {
     modalText.textContent = text;
 }
--- 2-JavaScript/Projects/TicTacToe/js/player.js
+++ 2-JavaScript/Projects/TicTacToe/js/player.js
@@ -2,5 +2,6 @@ export default class Player {
     constructor(name, symbol) {
         this.name = name;
         this.symbol = symbol;
+        this.points = 0;
     }
 }
