c6ec4cc0ce9b5c9151c0caecf597c750862d7cde
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-06-26 18:26:17 +0200

    Implemented The Draw Functions and Reset the Board

--- 2-JavaScript/Projects/TicTacToe/js/main.js
+++ 2-JavaScript/Projects/TicTacToe/js/main.js
@@ -1,6 +1,10 @@
 import Player  from "./player.js";
 import ticTacToe from "./tictactoe.js";
 
+// Elements
+const roundCounter = document.querySelector("#round-counter");
+const modalText = document.querySelector(".modal-text");
+
 const player1 = new Player("Player 1", "X");
 const player2 = new Player("Player 2", "O");
 let currentPlayer = player1;
@@ -10,9 +14,29 @@ tabs.forEach(tab => {
     tab.addEventListener("click", (e) => {
         const index = e.target.dataset.index;
         const card = e.target.children[0];
+        if (card.classList.length > 0) { // If the card already has a symbol, return
+            return;
+        }
+
         ticTacToe.placeCard(card, currentPlayer.symbol, index);
+        
+        if (ticTacToe.isDraw()) {
+            setModalText("Draw!");
+            ticTacToe.cleanBoard();
+            addRound();
+            return;
+        }
         currentPlayer = currentPlayer === player1 ? player2 : player1;
+
     });
 });
 
 ticTacToe.buildBoard();
+
+function addRound() {
+    roundCounter.textContent = Number(roundCounter.textContent) + 1;
+};
+
+function setModalText(text) {
+    modalText.textContent = text;
+}
--- 2-JavaScript/Projects/TicTacToe/js/tictactoe.js
+++ 2-JavaScript/Projects/TicTacToe/js/tictactoe.js
@@ -1,11 +1,18 @@
 const ticTacToe = (() => {
-    const gameBoard = new Array(9);
+    const gameBoard = new Array(9).fill(undefined);
     const symbols = {
         symbolClass: "material-symbols-outlined",
         circleTextContent: "circle",
         crossTextContent: "close",
     }
 
+    /*
+    * Place a card on the board
+    * @param {HTMLElement} card - The card to place
+    * @param {string} symbol - The symbol to place
+    * @param {number} index - The index of the card
+    * @returns {void}
+    */
     const placeCard = (card, symbol, index) => {
         if (card.classList.length > 0) {
             return;
@@ -26,6 +33,7 @@ const ticTacToe = (() => {
     const buildBoard = () => {
         const boardCards = document.querySelectorAll(".tab");
         for (let i = 0; i < 9; i++) {
+            boardCards[i].innerHTML = "";
             const card = document.createElement("span");
             switch (gameBoard[i]) {
                 case undefined: break;
@@ -38,7 +46,16 @@ const ticTacToe = (() => {
             boardCards[i].appendChild(card);
         }
     }
-    return { placeCard, buildBoard}
+
+    const cleanBoard = () => {
+        gameBoard.fill(undefined);
+        buildBoard();
+    }
+
+    const isDraw = () => { 
+        return gameBoard.every((card) => card !== undefined);
+    }
+    return { placeCard, buildBoard, isDraw, cleanBoard}
 })();
 
 export default ticTacToe;
