d7577c1ad3999380c73c629ffd03905701d2b91b
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-07-03 17:07:41 +0200

    Created banner module

---
+++ 2-JavaScript/Projects/RestaurantPage/src/modules/Home/banner/banner.js
@@ -0,0 +1,29 @@
+import '../../../css/banner.css';
+
+
+const subTitle = "Welcome to our Coffeeshop"
+const title = "Where Coffee Meets Hospitality"
+export default function createBanner() {
+    const banner = document.createElement('section');
+    banner.classList.add('banner');
+
+
+    const bannerText = document.createElement('div');
+    bannerText.classList.add('banner__text');
+
+    const bannerSubtitle = document.createElement('h2');
+    bannerSubtitle.classList.add('banner__subtitle');
+    bannerSubtitle.textContent = subTitle;
+
+    const bannerTitle = document.createElement('h1');
+    bannerTitle.classList.add('banner__title');
+    bannerTitle.textContent = title;
+
+    bannerText.appendChild(bannerSubtitle);
+    bannerText.appendChild(bannerTitle);
+    
+
+    banner.appendChild(bannerText);
+
+    return banner; 
+}
