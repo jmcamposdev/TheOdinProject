702c54cbef235fbe2ef15b1797cd5e79297faf03
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-07-04 17:34:14 +0200

    Created Footer module and implemented to the home.js file

--- 2-JavaScript/Projects/RestaurantPage/src/pages/home.js
+++ 2-JavaScript/Projects/RestaurantPage/src/pages/home.js
@@ -3,15 +3,19 @@ import createHeader from '../modules/Home/header/header.js';
 import createWhatWeDo from '../modules/Home/whatWeDo/whatWeDo.js';
 import {createCoffeeCulture, animateLetters} from '../modules/Home/coffeeCulture/coffeeCulture.js';
 import createAroma from '../modules/Home/aroma/aroma.js';
+import scrollToTop from '../assets/scrollToTop.js';
+import createFooter from '../modules/Home/footer/footer.js';
 
 function createHome() {
     const root = document.querySelector('#root');
-    root.appendChild(createHeader());
+    root.appendChild(scrollToTop())  
+    root.appendChild(createHeader()); 
     root.appendChild(createBanner());
     root.appendChild(createWhatWeDo());
     root.appendChild(createCoffeeCulture());
     animateLetters();
     root.appendChild(createAroma());
+    root.appendChild(createFooter());
 }
 
 export default createHome;
