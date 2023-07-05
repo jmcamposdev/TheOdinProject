7ddf39b3c6101c724f39dc4bc694675971133a03
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-07-05 15:43:03 +0200

    Remove all Main child before add the home elements

--- 2-JavaScript/Projects/RestaurantPage/src/pages/home.js
+++ 2-JavaScript/Projects/RestaurantPage/src/pages/home.js
@@ -1,13 +1,11 @@
 import createBanner from '../modules/Home/banner/banner.js';
-import createHeader from '../modules/Global/header/header.js';
 import createWhatWeDo from '../modules/Home/whatWeDo/whatWeDo.js';
 import {createCoffeeCulture, animateLetters} from '../modules/Home/coffeeCulture/coffeeCulture.js';
 import createAroma from '../modules/Home/aroma/aroma.js';
-import scrollToTop from '../assets/scrollToTop.js';
-import createFooter from '../modules/Global/footer/footer.js';
 
 function createHome() {
     const main = document.querySelector('#main');
+    main.textContent = '';
     main.appendChild(createBanner());
     main.appendChild(createWhatWeDo());
     main.appendChild(createCoffeeCulture());
