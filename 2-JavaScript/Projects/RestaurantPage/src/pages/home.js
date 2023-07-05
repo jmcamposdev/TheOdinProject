48c02e79a63600b1a23fdb2da484cc55e17dc551
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-07-05 12:50:03 +0200

    Move footer and header modules to Global folder

--- 2-JavaScript/Projects/RestaurantPage/src/pages/home.js
+++ 2-JavaScript/Projects/RestaurantPage/src/pages/home.js
@@ -1,10 +1,10 @@
 import createBanner from '../modules/Home/banner/banner.js';
-import createHeader from '../modules/Home/header/header.js';
+import createHeader from '../modules/Global/header/header.js';
 import createWhatWeDo from '../modules/Home/whatWeDo/whatWeDo.js';
 import {createCoffeeCulture, animateLetters} from '../modules/Home/coffeeCulture/coffeeCulture.js';
 import createAroma from '../modules/Home/aroma/aroma.js';
 import scrollToTop from '../assets/scrollToTop.js';
-import createFooter from '../modules/Home/footer/footer.js';
+import createFooter from '../modules/Global/footer/footer.js';
 
 function createHome() {
     const root = document.querySelector('#root');
