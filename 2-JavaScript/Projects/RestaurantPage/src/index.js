2b3763232b99edf9642f751a3d69f6837ecedd17
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-07-06 14:29:26 +0200

    Added Events to Menu items

--- 2-JavaScript/Projects/RestaurantPage/src/index.js
+++ 2-JavaScript/Projects/RestaurantPage/src/index.js
@@ -1,11 +1,33 @@
 import './css/global.css';
 import createHome from "./pages/home";
 import loadPage from './pages/loadPage';
+import createProducts from './pages/products';
 
+function addNavEvents ()  {
+    const homeBtn = document.querySelector('#home');
+    const menuBtn = document.querySelector('#menu');
+    const contactBtn = document.querySelector('#contact');
+
+    homeBtn.addEventListener('click', createHome);
+    menuBtn.addEventListener('click', createProducts);
+}
 
 init();
 
 function init() {
     loadPage();
     createHome();
+    addNavEvents();
 }
+
+function clearMain() {
+    const main = document.querySelector('#main');
+    main.textContent = '';
+}
+
+function addMain(element) {
+    const main = document.querySelector('#main');
+    main.appendChild(element);
+};
+
+export {clearMain, addMain};
