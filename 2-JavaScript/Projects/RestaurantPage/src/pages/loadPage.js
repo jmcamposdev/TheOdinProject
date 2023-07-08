3b7a7163eca5757728cc03e55c3af6afcd36ada5
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-07-08 15:40:42 +0200

    Fixed scrollToTop disappear when change the menu now its always able

--- 2-JavaScript/Projects/RestaurantPage/src/pages/loadPage.js
+++ 2-JavaScript/Projects/RestaurantPage/src/pages/loadPage.js
@@ -6,11 +6,12 @@ export default function loadPage() {
     const root = document.querySelector('#root');
 
     root.appendChild(createHeader()); // Header that is static for all pages
+
+    root.appendChild(scrollToTop()); // Scroll to top button that will be hidden until the user scrolls down
     
     // Main that will change depending on the page the user is on
     const main = document.createElement('main'); 
     main.setAttribute('id', 'main');
-    main.appendChild(scrollToTop()); // Scroll to top button that will be hidden until the user scrolls down
     root.appendChild(main);
 
     root.appendChild(createFooter()); // Footer that is static for all pages
