031b7027b5013aa776becefcfbf4f88c114903aa
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-07-19 15:56:03 +0200

    Started Added Header into html

--- 2-JavaScript/Projects/TodoList/src/index.js
+++ 2-JavaScript/Projects/TodoList/src/index.js
@@ -1,8 +1,2 @@
-import createHeader from "./modules/Header/Header";
-import { addRoot } from "./modules/Root/DOMRoot";
-
-function init () {
-    addRoot(createHeader());
-}
-
-init();
+import "./css/global.css"
+import "./css/header.css"
