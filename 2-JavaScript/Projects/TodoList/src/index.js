4658f8ca1612782cc641a91018770604f1670be5
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-07-21 22:19:13 +0200

    Change the string to boolean

--- 2-JavaScript/Projects/TodoList/src/index.js
+++ 2-JavaScript/Projects/TodoList/src/index.js
@@ -6,8 +6,8 @@ import DOMTodoListClass from "./modules/TodoList/DOMTodoList"
 
 function init()  {
     const DOMTodoList = new DOMTodoListClass();
-    DOMTodoList.addTask(new Task('Task 3', 'This is the third task', '2021-01-03', 'true', 'inbox'));
-    DOMTodoList.addTask(new Task('Task 4', 'This is the fourth task', '2021-01-04', 'false', 'inbox'));
+    DOMTodoList.addTask(new Task('Task 3', 'This is the third task', '2021-01-03', true, 'inbox'));
+    DOMTodoList.addTask(new Task('Task 4', 'This is the fourth task', '2021-01-04', false, 'inbox'));
 
     DOMTodoList.createAddTaskElement();
 }
