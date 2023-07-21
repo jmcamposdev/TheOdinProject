0617a9c4a30f0262ad392621f05bc3932b4788b2
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-07-21 22:19:52 +0200

    Implemented checked or unchecked icon depending if is completed or not

--- 2-JavaScript/Projects/TodoList/src/modules/Task/DOMTask.js
+++ 2-JavaScript/Projects/TodoList/src/modules/Task/DOMTask.js
@@ -7,9 +7,10 @@ export default class DOMTask {
      */
     static printTaskElement (task) {
         const taskList = document.querySelector('.task-list');
+        let checkIcon = task.getCompleted() ? 'check_circle' : 'radio_button_unchecked';
         const taskElement = `
             <div class="task">
-                <span class="material-symbols-outlined">radio_button_unchecked</span>
+                <span class="material-symbols-outlined">${checkIcon}</span>
                 <p class="task-title">${task.getTitle()}</p>
             </div>`;
         taskList.insertAdjacentHTML('beforeend', taskElement);
