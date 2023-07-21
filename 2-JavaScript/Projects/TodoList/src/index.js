bb1ac4510b2ba25c874e907e4b2c56724edf55a7
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-07-21 21:01:20 +0200

    Task Functionality Add Task Form

--- 2-JavaScript/Projects/TodoList/src/index.js
+++ 2-JavaScript/Projects/TodoList/src/index.js
@@ -1,2 +1,6 @@
 import "./css/global.css"
 import "./css/header.css"
+import "./css/main.css"
+import DOMTask from "./modules/Task/DOMTask"
+
+DOMTask.createAddTaskElement();
---
+++ 2-JavaScript/Projects/TodoList/src/modules/Task/Task.js
@@ -0,0 +1,84 @@
+export default class Task {
+    constructor (title, description, dueDate, completed, project) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.completed = completed;
+        this.project = project;
+    }
+
+    getTitle () {
+        return this.title;
+    }
+    setTitle (title) {
+        this.title = title;
+    }
+
+    getDescription () {
+        return this.description;
+    }
+    setDescription (description) {
+        this.description = description;
+    }
+
+    getDueDate () {
+        return this.dueDate;
+    }
+    setDueDate (dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    getCompleted () {
+        return this.completed;
+    }
+    setCompleted (completed) {
+        this.completed = completed;
+    }
+
+    toggleCompleted () {
+        this.completed = !this.completed;
+    }
+
+    getProject () {
+        return this.project;
+    }
+    setProject (project) {
+        this.project = project;
+    }
+
+    toString () {
+        return `Title: ${this.title}\nDescription: ${this.description}\nDue Date: ${this.dueDate}\nCompleted: ${this.completed}`;
+    }
+
+    static fromJSON (json) {
+        return new Task(json.title, json.description, json.dueDate, json.completed);
+    }
+
+    static fromJSONList (jsonList) {
+        let taskList = [];
+        for (let json of jsonList) {
+            taskList.push(Task.fromJSON(json));
+        }
+        return taskList;
+    }
+
+    static toJSON (task) {
+        return JSON.stringify(task);
+    }
+
+    static toJSONList (taskList) {
+        let jsonList = [];
+        for (let task of taskList) {
+            jsonList.push(Task.toJSON(task));
+        }
+        return jsonList;
+    }
+
+    static toLocalStorage (taskList) {
+        localStorage.setItem('taskList', Task.toJSONList(taskList));
+    }
+
+    static fromLocalStorage () {
+        return Task.fromJSONList(JSON.parse(localStorage.getItem('taskList')));
+    }
+}
