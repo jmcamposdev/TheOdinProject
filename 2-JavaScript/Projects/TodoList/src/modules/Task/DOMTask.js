6031a209722d97bd961f4db7bb11520a01e9f473
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-07-28 12:00:00 +0200

    Development checkbox eventListener

--- 2-JavaScript/Projects/TodoList/src/modules/Task/DOMTask.js
+++ 2-JavaScript/Projects/TodoList/src/modules/Task/DOMTask.js
@@ -25,23 +25,33 @@ export default class DOMTask {
         const taskElement = document.createElement("div");
         taskElement.classList.add("task");
         taskElement.dataset.id = task.getId();
-        taskElement.innerHTML = `
-            <div class="task-info">
-                ${DOMTask.createCheckBoxElement(task.isCompleted())}
-                <div class="extra-data">
-                    ${task.getDueDate()
-                    ? DOMTask.createDueDateElement(task.getDueDate())
-                    : ""}
-                </div> 
-                <p class="task-title">${task.getTitle()}</p>
-            </div>
-            <div class="task-delete-container">
-                <span class="delete-icon material-symbols-outlined">delete</span>
-            </div>`;
 
-        DOMTask.addCheckBoxEventListener(task.getId(), taskElement); // Add the event listener to the checkbox to toggle the completed state of the task
-        DOMTask.addRemoveTaskEventListener(taskElement); // Add the event listener to the delete icon to remove the task from the task list
-        DOMTask.addEditTaskEventListener(taskElement); // Add the event listener to the task element to edit the task
+        const taskInfoElement = document.createElement("div");
+        taskInfoElement.classList.add("task-info");
+
+        const checkBoxElement = DOMTask.createCheckBoxElement(task.isCompleted());
+        taskInfoElement.appendChild(checkBoxElement);
+
+        taskInfoElement.innerHTML += `
+            <div class="extra-data">
+                ${task.getDueDate()
+                ? DOMTask.createDueDateElement(task.getDueDate())
+                : ""}
+            </div> 
+            <p class="task-title">${task.getTitle()}</p>`;
+        
+        const taskDeleteContainer = document.createElement("div");
+        taskDeleteContainer.classList.add("task-delete-container");
+        taskDeleteContainer.innerHTML = `
+            <span class="delete-icon material-symbols-outlined">delete</span>`;
+
+        taskElement.appendChild(taskInfoElement);
+        taskElement.appendChild(taskDeleteContainer);
+
+
+        //DOMTask.addCheckBoxEventListener(task.getId(), taskElement); // Add the event listener to the checkbox to toggle the completed state of the task
+        // DOMTask.addRemoveTaskEventListener(taskElement); // Add the event listener to the delete icon to remove the task from the task list
+        // DOMTask.addEditTaskEventListener(taskElement); // Add the event listener to the task element to edit the task
         return taskElement;
     }
 
@@ -51,9 +61,12 @@ export default class DOMTask {
     }
 
     static createCheckBoxElement(isChecked) {
-        const checkIcon = isChecked ? 'check_circle' : 'radio_button_unchecked';
-        const checkElement = `<span data-is-completed="${isChecked}" class="check-box material-symbols-outlined">${checkIcon}</span>`;
-        return checkElement;
+        const checkBoxElement = document.createElement("span");
+        checkBoxElement.setAttribute("data-is-completed", `${isChecked}`)
+        checkBoxElement.classList.add("check-box", "material-symbols-outlined");
+        checkBoxElement.textContent = isChecked ? 'check_circle' : 'radio_button_unchecked';
+        checkBoxElement.addEventListener("click", DOMTask.updateCheckBoxElement(checkBoxElement, !isChecked));
+        return checkBoxElement;
     }
 
     /**
