eb427ae7628827ca7fbfd4b6d296ab8fc12a04f9
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-08-05 12:53:05 +0200

    Implemented placeholder to date input

--- 2-JavaScript/Projects/TodoList/src/modules/TodoList/DOMTodoList.js
+++ 2-JavaScript/Projects/TodoList/src/modules/TodoList/DOMTodoList.js
@@ -240,7 +240,7 @@ export default class DOMTodoList {
                         <div class="optional-data">
                             <div class="due-date-container">
                                 <span class="due-date-icon material-symbols-outlined">flag</span>
-                                <input type="date" class="new-task-due-date" value="${taskDueDate}" min=${firstDateOfWeek} max="${lastDateOfWeek}">
+                                <input type="date" class="new-task-due-date" placeholder="dd/mm/yyyy" value="${taskDueDate}" min="${firstDateOfWeek}" max="${lastDateOfWeek}">
                             </div>
                             <div class="tags-container">
                             <span class="tag-icon material-symbols-outlined">sell</span>
