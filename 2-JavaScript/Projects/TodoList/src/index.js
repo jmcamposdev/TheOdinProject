a59e5e7d8577746fa4f8893812e096d78a868f5c
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-08-06 14:52:06 +0200

    Convert categories string to array

--- 2-JavaScript/Projects/TodoList/src/index.js
+++ 2-JavaScript/Projects/TodoList/src/index.js
@@ -12,8 +12,8 @@ import { createHamburgerNavEvent } from "./assets/Hamburger/hamburgerNav"
 
 const DOMTodoList = new DOMTodoListClass();
 function init()  {
-    DOMTodoList.addTask(new Task('Task 3', 'This is the third task', new Date(2021, 0, 3, 0, 0, 0, 0), true, 'inbox', "Home"));
-    DOMTodoList.addTask(new Task('Task 4', 'This is the fourth task', new Date(2021, 0, 3, 0, 0, 0, 0), false, 'inbox', "Outdoors"));
+    DOMTodoList.addTask(new Task('Task 3', 'This is the third task', new Date(2021, 0, 3, 0, 0, 0, 0), true, 'inbox', ["Home"]));
+    DOMTodoList.addTask(new Task('Task 4', 'This is the fourth task', new Date(2021, 0, 3, 0, 0, 0, 0), false, 'inbox', ["Outdoors"]));
     DOMTodoList.printAllTasks();
     DOMTodoList.createProjectsEvents();
     printAddProjectsElement();
