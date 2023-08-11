13c7b8b814d3b18fd4f71c6bd3c54d13de482252
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-08-11 16:58:06 +0200

    Implementing html-webpack-plugin

--- 2-JavaScript/Projects/Weather/webpack.config.js
+++ 2-JavaScript/Projects/Weather/webpack.config.js
@@ -1,16 +1,31 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true, // clean the dist folder before each build
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
     ],
   },
