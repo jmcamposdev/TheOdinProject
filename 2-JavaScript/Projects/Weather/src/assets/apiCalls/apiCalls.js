3da2486204c4cf37bb8e8af0d1393230a285497b
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-08-12 21:04:16 +0200

    Moved to separated file

--- 2-JavaScript/Projects/Weather/src/assets/apiCalls/apiCalls.js
+++ 2-JavaScript/Projects/Weather/src/assets/apiCalls/apiCalls.js
@@ -1,3 +1,4 @@
+import setLoading from '../utils/setLoading';
 /**
  * Return the weather for the given location
  * Use the weatherapi.com API
@@ -7,7 +8,13 @@
  * return {object} weather
  */
 export default async function getWeatherData(location) {
+  setLoading(true);
+  /* eslint max-len: ["error", { "ignoreUrls": true }] */
   const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHER_API}&q=${location}&days=3&aqi=yes&alerts=no`);
   const data = await response.json();
+  if (!data.error) {
+    setLoading(false);
+  }
+
   return data;
 }
