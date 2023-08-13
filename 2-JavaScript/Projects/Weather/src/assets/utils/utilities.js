5da6bfd6e8eca3895aba0180bba54538794250d7
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-08-13 16:47:48 +0200

    Created utilities file for general functions

---
+++ 2-JavaScript/Projects/Weather/src/assets/utils/utilities.js
@@ -0,0 +1,13 @@
+/**
+ * Return the day of the week for the given date.
+ * E.g. Monday, Tuesday, etc.
+ * @param {Date} date
+ * @return {string}
+ */
+function getDay(date) {
+  console.log(date);
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  return days[date.getDay()];
+}
+
+export {getDay};
