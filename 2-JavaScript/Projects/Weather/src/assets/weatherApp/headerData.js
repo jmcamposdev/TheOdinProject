659aa511f57ae629664dd1edf05a02c8f8eb8439
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-08-13 16:47:55 +0200

    Refactor getDay

--- 2-JavaScript/Projects/Weather/src/assets/weatherApp/headerData.js
+++ 2-JavaScript/Projects/Weather/src/assets/weatherApp/headerData.js
@@ -1,3 +1,5 @@
+import {getDay} from '../utils/utilities';
+
 /**
  * Print in the Header the Date and the City
  * @param {Object} location
@@ -12,11 +14,10 @@ function printHeaderData(location) {
   const monthElement =
   document.querySelector('.header__city__name__date__textMonth');
   // Setting the content
-  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   const date = new Date(location.localtime);
 
   nameCityElement.textContent = location.name; // City Name
-  textDayElement.textContent = days[date.getDay()]; // Day of the Week
+  textDayElement.textContent = getDay(date); // Day of the Week
   numberDayElement.textContent = date.getDate(); // Day of the Month
   monthElement.textContent = date.toLocaleString('en-us', {month: 'long'}); // Month
 }
