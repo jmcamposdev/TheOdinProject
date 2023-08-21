699471d4fe7da730d1f1a98d1e0950705ca2c927
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-08-21 22:33:56 +0200

    Starting Fibonacci Project

---
+++ 2-JavaScript/Projects/Fibonacci/fibonacci.js
@@ -0,0 +1,10 @@
+/**
+ * Calculate the Fibonacci sequence
+ * @param {Number} numIteration 
+ * @returns {Array} Returns an array with the length of numIteration and the Fibonacci sequence
+ */
+function fibs (numIteration) {
+  console.log('Calculating Fibonacci sequence...');
+}
+
+fibs(10); // Expected Result --> [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
