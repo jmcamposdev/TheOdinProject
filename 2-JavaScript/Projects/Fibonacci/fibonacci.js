2c99e003b20c8d79df3e46dabf1a7c38db4790aa
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-08-22 19:04:01 +0200

    Fibonacci iterative function

--- 2-JavaScript/Projects/Fibonacci/fibonacci.js
+++ 2-JavaScript/Projects/Fibonacci/fibonacci.js
@@ -4,7 +4,27 @@
  * @returns {Array} Returns an array with the length of numIteration and the Fibonacci sequence
  */
 function fibs (numIteration) {
-  console.log('Calculating Fibonacci sequence...');
+  if (numIteration <= 0)  { // If numIteration is less than or equal to 0, return an empty array
+    return [];
+  }
+  if (numIteration == 1) { // If numIteration is equal to 1, return an array with 0
+    return [0];
+  }
+
+  // If numIteration is greater than 1, calculate the Fibonacci sequence
+  const fibSequence = [0, 1]; // Initialize the array with the first two numbers of the Fibonacci sequence
+  let index = 0; // Initialize the index to 0
+  for (let i = numIteration; i > 0; i--) { // Loop through the numIteration
+    // Add the sum of the last two numbers of the Fibonacci sequence to the array
+    fibSequence.push(fibSequence[index] + fibSequence[index++ + 1]);
+  }
+  
+  return fibSequence;
 }
 
-fibs(10); // Expected Result --> [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+fibs(0); // Expected Result --> []
+fibs(1); // Expected Result --> [0]
+fibs(2); // Expected Result --> [0, 1]
+fibs(3); // Expected Result --> [0, 1, 1]
+fibs(4); // Expected Result --> [0, 1, 1, 2]
+fibs(8); // Expected Result --> [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
