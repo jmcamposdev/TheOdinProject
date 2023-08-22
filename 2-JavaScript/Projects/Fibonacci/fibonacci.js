ddfaf6199ea24fad90f68296211de39aa31dd322
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-08-22 19:21:18 +0200

    Optimization of Fibonacci function

--- 2-JavaScript/Projects/Fibonacci/fibonacci.js
+++ 2-JavaScript/Projects/Fibonacci/fibonacci.js
@@ -4,22 +4,20 @@
  * @returns {Array} Returns an array with the length of numIteration and the Fibonacci sequence
  */
 function fibs (numIteration) {
+  const sequence = [0, 1];
   if (numIteration <= 0)  { // If numIteration is less than or equal to 0, return an empty array
-    return [];
+    return 'Insert a positive number';
   }
   if (numIteration == 1) { // If numIteration is equal to 1, return an array with 0
-    return [0];
+    return sequence.splice(0,1);
   }
 
-  // If numIteration is greater than 1, calculate the Fibonacci sequence
-  const fibSequence = [0, 1]; // Initialize the array with the first two numbers of the Fibonacci sequence
-  let index = 0; // Initialize the index to 0
-  for (let i = numIteration; i > 0; i--) { // Loop through the numIteration
+  for (let i = numIteration; i > 2; i--) { // Loop through the numIteration
     // Add the sum of the last two numbers of the Fibonacci sequence to the array
-    fibSequence.push(fibSequence[index] + fibSequence[index++ + 1]);
+    sequence.push(sequence[sequence.length - 2] + sequence[sequence.length - 1]);
   }
 
-  return fibSequence;
+  return sequence;
 }
 
 console.log(fibs(0)); // Expected Result --> []
