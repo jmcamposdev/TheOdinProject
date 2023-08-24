9e41e822c92ca3a8a446f10011b65020138fa49a
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-08-24 22:01:32 +0200

    Added first extra recursion exercise

---
+++ 2-JavaScript/Projects/Recursion/Extra/multiples.js
@@ -0,0 +1,12 @@
+function multiplesOf3and5(number) {
+  number--;
+  if (number == 0) return 0;
+  if (number % 3 == 0 || number % 5 == 0) {
+    return number + multiplesOf3and5(number);
+  }
+  return multiplesOf3and5(number);
+}
+
+console.log(multiplesOf3and5(10)); // Should return 23
+console.log(multiplesOf3and5(1000)); // Should return 233168
+console.log(multiplesOf3and5(8456)); // Should return 16687353
