2cb71e282102a921abaad299cdaafbf540f08d2f
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-08-30 22:37:02 +0200

    Started the function

---
+++ 2-JavaScript/Projects/Recursion/Extra/largestPrimeFactor.js
@@ -0,0 +1,13 @@
+/**
+  * Calculate the largest prime factor of a given number.
+  * @param {number} number
+  * @return {number} largest prime factor of a given number.
+ */
+function largestPrimeFactor(number) {
+
+}
+
+// Test cases
+console.log(largestPrimeFactor(2)); // Should return 2
+console.log(largestPrimeFactor(3)); // Should return 3
+console.log(largestPrimeFactor(13195)) // Should return 29.
