64f961e7299330f83a8f84e3ad7190f3b5f73e38
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-08-31 15:24:12 +0200

    Added isPrime function

--- 2-JavaScript/Projects/Recursion/Extra/largestPrimeFactor.js
+++ 2-JavaScript/Projects/Recursion/Extra/largestPrimeFactor.js
@@ -4,10 +4,28 @@
   * @return {number} largest prime factor of a given number.
  */
 function largestPrimeFactor(number) {
-
+  if (number <= 0) {
+    return "Please enter a positive number.";
+  }
 }
 
 // Test cases
 console.log(largestPrimeFactor(2)); // Should return 2
 console.log(largestPrimeFactor(3)); // Should return 3
 console.log(largestPrimeFactor(13195)) // Should return 29.
+
+function isPrime(number) {
+  let isPrime = true;
+
+  for (let i = 2; i < number && isPrime; i++) {
+    if (number % i == 0) {
+      isPrime = false;
+    }
+  }
+  return isPrime;
+} 
+
+console.log(isPrime(2)); // Should return true
+console.log(isPrime(3)); // Should return true
+console.log(isPrime(4)); // Should return false
+console.log(isPrime(5)); // Should return true
