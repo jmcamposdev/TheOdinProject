b97a3e05fa9c476ae6de56007e695e525a6af50e
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-09-06 21:37:23 +0200

    Added some comments

--- 2-JavaScript/Projects/Recursion/Extra/isPrime.js
+++ 2-JavaScript/Projects/Recursion/Extra/isPrime.js
@@ -4,12 +4,13 @@
  * @return {Boolean} true if the number is prime, false otherwise.
  */
 function isPrime(number) {
-  let isPrime = true;
+  let isPrime = true; // Assume the number is prime
 
+  // Loop through all numbers from 2 to number/2 and check if the number is divisible by any of them
   for (let i = 2; i < number/2 && isPrime; i++) {
-    if (number % i == 0) {
-      isPrime = false;
+    if (number % i == 0) { // If the number is divisible by any number from 2 to number/2, it is not prime
+      isPrime = false; // Set isPrime to false
     }
   }
-  return isPrime;
+  return isPrime; // Return the result
 } 
